0aa04b01fafad9455713368c14946cdd94f7c2bf
Andrew Holway <andrew.holway@brickblock.io>
2019-02-18 08:14:41 +0100

    Revert "testing backend call"
    This reverts commit ca0f42dd751bf7ce0d7ec27958e5dbd373d723e0.

--- src/main.js
+++ src/main.js
@@ -1,17 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
-import axios from 'axios'
 
 new Vue({
   el: '#app',
-  data () {
-    return {
-      info: null
-    }
-  },
-  mounted () {
-    axios
-        .get('/api/test')
-        .then(response => (this.info = response.data))
-  }
+  render: h => h(App)
 })
