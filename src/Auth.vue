d471cc8caaeb195273f258681b92802eded94a0e
Andrey Oleynikov <my_olein@mail.ru>
2019-03-01 15:22:37 +0300

    Remove any callback form $auth call

--- src/Auth.vue
+++ src/Auth.vue
@@ -1,14 +1,12 @@
 <template>
   <div>
     <button @click="auth('google')" class="button--google">Auth google</button>
-    Response?: {{response}}
   </div>
 </template>
 <script>
 export default {
   data() {
     return {
-      response: ''
     }
   },
   methods: {
@@ -16,19 +14,7 @@ export default {
       if (this.$auth.isAuthenticated()) {
         this.$auth.logout()
       }
-
-      this.response = null
-
-      var this_ = this;
-      this.$auth.authenticate(provider).then(function (authResponse) {
-        if (provider === 'google') {
-          this_.$http.get('https://www.googleapis.com/plus/v1/people/me/openIdConnect').then(function (response) {
-            this_.response = response
-          })
-        }
-      }).catch(function (err) {
-        this_.response = err
-      })
+      this.$auth.authenticate(provider)
     }
   }
 }
